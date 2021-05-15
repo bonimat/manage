601737cbb3bdb66e39c4e1f7382abbc0198e59c8
Matteo Boni <bonimat@hotmail.com>
2021-05-15 11:25:39 +0200

    WIP: coding setrole

--- cmds/setrole.js
+++ cmds/setrole.js
@@ -1,10 +1,13 @@
 exports.command = 'setrole';
 exports.description = 'Set the role of user';
 exports.builder = (yargs) => {
-    return yargs.option('role', {
-        alias: 'r',
-        nargs: 1,
-        demandOption: true,
-    });
+    return yargs
+        .option('email', { alias: 'e', nargs: 1, demandOption: true })
+        .option('role', { alias: 'r', nargs: 1, demandOption: true })
+        .usage(
+            'Usage: $0 setrole [-e|--email] <username@mail.org> -r [admin|user]'
+        );
 };
-exports.handler = function (argv) {};
+exports.handler = function (argv) {
+    console.log(argv);
+};
