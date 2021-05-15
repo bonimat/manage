dc931676dd1a65a04ab298195c3389de93928bb6
Matteo Boni <bonimat@hotmail.com>
2021-05-15 11:17:12 +0200

    WIP: coding adduser

--- cmds/adduser.js
+++ cmds/adduser.js
@@ -1,9 +1,17 @@
+const { demandOption } = require('yargs');
+
 exports.command = 'adduser';
 exports.desc = 'Add new user';
-exports.builder = {
-    firstname: { alias: 'fn' },
-    lastname: { alias: 'ln' },
+exports.builder = (yargs) => {
+    return yargs
+        .option('username', { alias: 'u', nargs: 1, demandOption: true })
+        .option('firstname', { alias: 'n', nargs: 1, demandOption: true })
+        .option('lastname', { alias: 'l', nargs: 1, demandOption: true })
+        .option('email', { alias: 'e', nargs: 1, demandOption: true })
+        .usage(
+            'Usage: $0 adduser -u supermario -n mario -l rossi -e m.rossi@mail.it'
+        );
 };
 exports.handler = function (argv) {
-    console.log('and the user is...', argv.dir);
+    console.log(argv);
 };
